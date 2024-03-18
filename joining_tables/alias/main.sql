f80f6fbc4f1bf765a223f9c975771d0dbc6270d1
Cho Phan <callmechophan@gmail.com>
2024-03-18 18:58:40 +0700

    1.2.1 joining tables (AS)

---
+++ joining_tables/alias/main.sql
@@ -0,0 +1,13 @@
+/*
+ * AS
+ * renaming
+ * - rename tables or columns
+ * SELECT column1 AS column1_alias, column2, column3
+ * 
+ * 
+ * function
+ * - combine column outputs using concatenation function
+ *
+ * multiple tables
+ * - use an alias to create distinct table names
+*/
