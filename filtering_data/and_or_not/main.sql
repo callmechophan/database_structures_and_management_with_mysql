728abca085af4c5d57805d7e78a9afb05ce0f36f
Cho Phan <callmechophan@gmail.com>
2024-04-02 22:33:08 +0700

    1.1.1 AND, OR, NOT

---
+++ filtering_data/and_or_not/main.sql
@@ -0,0 +1,19 @@
+/*
+ * AND
+ * - all combined conditions meet the value of "true"
+ * SELECT *
+ * FROM table_name
+ * WHERE condition1 AND condition2 AND condition3...;
+ *
+ * OR
+ * - at least one condition the value of true
+ * SELECT *
+ * FROM table_name
+ * WHERE condition1 OR condition2 OR condition3...;
+ *
+ * NOT
+ * - are not true
+ * SELECT *
+ * FROM table_name
+ * WHERE NOT condition;
+*/
