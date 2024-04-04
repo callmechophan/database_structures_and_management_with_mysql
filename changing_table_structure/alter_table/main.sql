99627fea392b1be7064b3c7cd10c96d28d86ed96
Cho Phan <callmechophan@gmail.com>
2024-04-05 02:40:45 +0700

    2.3.1 ALTER TABLE

--- changing_table_structure/alter_table/main.sql
+++ changing_table_structure/alter_table/main.sql
@@ -5,17 +5,17 @@
  * MODIFY
  * - make change to specific columns in a table
  * ALTER TABLE table_name
- * MODIFY column1_name VARCHAR(10) NOT NULL
- * MODIFY column2_name CHAR(10) NOT NULL
- * MODIFY column3_name INT NOT NULL;
+ * MODIFY [COLUMN] column1_name VARCHAR(10) NOT NULL
+ * MODIFY [COLUMN] column2_name CHAR(10) NOT NULL
+ * MODIFY [COLUMN] column3_name INT NOT NULL;
  * 
  * ADD
  * - add a column to a table
  * ALTER TABLE table_name
- * ADD COLUMN column_name;
+ * ADD [COLUMN] column_name;
  *
  * DROP
  * - drop a column from a table
  * ALTER TABLE table_name
- * DROP COLUMN column_name;
+ * DROP [COLUMN] column_name;
 */
