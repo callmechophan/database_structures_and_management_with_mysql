485a51422efe4c5747cedc28016368d88cb11e21
Cho Phan <callmechophan@gmail.com>
2024-04-05 02:19:40 +0700

    2.3.1 ALTER TABLE

---
+++ changing_table_structure/alter_table/main.sql
@@ -0,0 +1,21 @@
+/*
+ * ALTER TABLE
+ * - make changes to a table in a database by altering column or constraints 
+ * 
+ * MODIFY
+ * - make change to specific columns in a table
+ * ALTER TABLE table_name
+ * MODIFY column1_name VARCHAR(10) NOT NULL
+ * MODIFY column2_name CHAR(10) NOT NULL
+ * MODIFY column3_name INT NOT NULL;
+ * 
+ * ADD
+ * - add a column to a table
+ * ALTER TABLE table_name
+ * ADD COLUMN column_name;
+ *
+ * DROP
+ * - drop a column from a table
+ * ALTER TABLE table_name
+ * DROP COLUMN column_name;
+*/
