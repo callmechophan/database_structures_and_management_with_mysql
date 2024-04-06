181b3a40758bcac5b4abd11ee5c04d9c66ace2fe
Cho Phan <callmechophan@gmail.com>
2024-04-06 11:54:51 +0700

    3.1.1 numeric functions

---
+++ functions/numeric_functions/main.sql
@@ -0,0 +1,26 @@
+/*
+ * MYSQL functions
+ * - a code that performs an operation and returns a result, often with the use of parameter
+ * 
+ * MYSQL functions
+ * - numeric functions
+ * - string functions
+ * - date functions
+ * - comparison functions
+ * - control flow functions
+ * 
+ * numeric functions
+ * - aggregate: perform tasks on a set of values
+ * - math: perform basic mathmatical tasks on data
+ * 
+ * aggregate functions
+ * - SUM()
+ * - AVG()
+ * - MAX()
+ * - MIN()
+ * - COUNT()
+ * 
+ * math functions
+ * - ROUND()
+ * - MOD()
+*/
