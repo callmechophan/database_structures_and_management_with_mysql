4c9fab4cc672f92f88ea4e8174c6bb0344282f2d
Cho Phan <callmechophan@gmail.com>
2024-04-06 15:21:16 +0700

    3.1.5 control flow functions

---
+++ functions/control_flow_functions/main.sql
@@ -0,0 +1,7 @@
+/*
+ * control flow functions
+ * - evaluate conditions and determine the execution path of a query
+ * 
+ * CASE
+ * - runs through a set of conditions contained within a CASE block and returns a value when the first condition is met
+*/
